f8ecbfb05a9779e4a34ce73fa8d7789cc056b24a
Maximilian Schmitt <maximilian.schmitt@googlemail.com>
2014-03-08 22:52:56 +0100

    Initial commit

---
+++ shortcodeparser.plugin.js
@@ -0,0 +1,33 @@
+module.exports = function(BasePlugin) {
+
+	var shortcode = require('shortcode-parser');
+
+	var Plugin = BasePlugin.extend({
+
+		name: 'easyshortcodes',
+
+		config: {
+			codes: [],
+			extension: 'sc'
+		},
+
+		constructor: function() {
+			Plugin.__super__.constructor.apply(this, arguments);
+
+			this.config.codes.forEach(function(code) {
+				shortcode.add(code.tag, code.cb);
+			});
+		},
+
+
+		render: function(opts, next) {
+			if (opts.inExtension !== this.config.extension) return next();
+
+			opts.content = shortcode.parse(opts.content, opts.templateData.document);
+
+			return next();
+		}
+	});
+
+	return Plugin;
+};
